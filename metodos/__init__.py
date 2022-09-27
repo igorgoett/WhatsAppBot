01abb8bc253009ca7a508702fad97cfa90934625
Igor Goettenauer <igorgoettenauer@gmail.com>
2022-09-27 12:28:53 -0300

    Separando msg do source

--- metodos/__init__.py
+++ metodos/__init__.py
@@ -4,6 +4,7 @@ import pyautogui
 import time
 from tkinter import *
 from contacts import first_names, new_phones
+import msg
 
 win = Tk() # Some Tkinter stuff
 screen_width = win.winfo_screenwidth() # Gets the resolution (width) of your monitor
@@ -12,9 +13,8 @@ cont = 0
 
 
 while cont < len(first_names):
-    msg = f"Bom dia Sr.(a) {first_names[cont]}\nMeu nome é Jenniffer, sou da Construtora Você, gostaria de saber se o Sr. (a) está tendo alguma dúvida em relação ao seu Financiamento Imobiliário? Possui aplicativo de Habitação Caixa? Está tendo alguma dificuldade no seu relacionamento com a CAIXA ECONÔMICA FEDERAL?\nE em relação as suas parcelas em aberto conosco, gostaria de verificar se o senhor (a) possui alguma previsão para pagamento?\nCaso já tenha realizado o pagamento, gentileza desconsiderar a mensagem acima.\nAtenciosamente\n\nRelacionamento com o Cliente\n"
 
-    pywhatkit.sendwhatmsg_instantly(new_phones[cont], msg) # Sends the message
+    pywhatkit.sendwhatmsg_instantly(new_phones[cont], msg.msg(first_names,cont)) # Sends the message
     cont += 1
     pyautogui.moveTo(screen_width * 0.694, screen_height* 0.900) # Moves the cursor the the message bar in Whatsapp || CHANGE HEIGHT ACCORDING
     time.sleep(5)
