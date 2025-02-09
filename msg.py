6173c87966faa06e4e6a3e8c6e57ab61f4033187
Igor Goettenauer <109356624+igorgoett@users.noreply.github.com>
2025-02-09 19:54:52 +0100

    Update msg.py

--- msg.py
+++ msg.py
@@ -2,9 +2,9 @@ from datetime import datetime
 
 
 def opcao_msg(names, index,opcao,nome_atendimento):
-    adimplente = f"{time} Sr.(a) {names[index]}\nMeu nome é {nome_atendimento}, sou da Construtora Você, o motivo da mensagem é para saber se o (a) Sr. (a) está tendo alguma dúvida em relação ao seu Financiamento Imobiliário?\nPossui aplicativo de Habitação Caixa?\nEstá tendo alguma dificuldade no seu relacionamento com a CAIXA ECONÔMICA FEDERAL?"
+    adimplente = f"{time} Sr.(a) {names[index]}\nMeu nome é {nome_atendimento}, "
 
-    inadimplente = f"{time} Sr.(a) {names[index]}\nMeu nome é {nome_atendimento}, sou da Construtora Você, gostaria de saber se o Sr. (a) está tendo alguma dúvida em relação ao seu Financiamento Imobiliário? Possui aplicativo de Habitação Caixa? Está tendo alguma dificuldade no seu relacionamento com a CAIXA ECONÔMICA FEDERAL?\nE em relação as suas parcelas em aberto conosco, gostaria de verificar se o senhor (a) possui alguma previsão para pagamento?\nCaso já tenha realizado o pagamento, gentileza desconsiderar a mensagem acima."
+    inadimplente = f"{time} Sr.(a) {names[index]}\nMeu nome é {nome_atendimento}, 
 
     if opcao == '1':
         text = inadimplente
